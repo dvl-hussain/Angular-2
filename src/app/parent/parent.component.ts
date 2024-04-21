16fe2df8639545f2bd4ae721c30c5ee09b6b61f8
dvl-hussain <husian.shah@digivatelabs.com>
2024-04-21 22:58:38 +0530

    view encapsulation update

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,10 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [ChildComponent],
+  imports: [],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css',
   encapsulation:ViewEncapsulation.ShadowDom
