4922654a84bf51bf5a73f5e3fe725ca34a2e643a
dvl-hussain <husian.shah@digivatelabs.com>
2024-04-21 14:58:56 +0530

    changes updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CustomDirective } from './Directive/custom.directive';
 import { TemplateProjectionComponent } from './template-projection/template-projection.component';
+import { TemplateContainerComponent } from './template-container/template-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,CustomDirective,TemplateProjectionComponent],
+  imports: [RouterOutlet,FormsModule,CustomDirective,TemplateProjectionComponent,TemplateContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/template-projection/template-projection.component.ts
+++ src/app/template-projection/template-projection.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template-projection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './template-projection.component.html',
   styleUrl: './template-projection.component.css'
 })
