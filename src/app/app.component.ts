19d37c5d39eb139dbfbff9000360e531be8336a0
dvl-hussain <husian.shah@digivatelabs.com>
2024-04-23 14:14:44 +0530

    view child assignment updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ParentComponent } from './parent/parent.component';
@@ -10,6 +10,51 @@ import { ParentComponent } from './parent/parent.component';
   styleUrl: './app.component.css',
   encapsulation:ViewEncapsulation.Emulated
 })
-export class AppComponent {
-  prop:string = ''
+export class AppComponent implements AfterViewInit {
+
+  prop:string = 'sending';
+
+
+
+  @ViewChild(ParentComponent)parent!:ParentComponent;
+  @ViewChild('parentReference',{read:ElementRef})parent_2!:ElementRef;
+  @ViewChild('title')
+  title!: ElementRef;
+   reg = new RegExp(/[a-zA-Z]+/g)
+   text = "how's life is 35going on?";
+
+  ngAfterViewInit(): void {
+    this.parent.log();
+    console.log(this.parent_2.nativeElement)
+    this.parent_2.nativeElement.style.color = 'green'
+    this.title.nativeElement.style.color = 'blue'
+    console.log("title:",  this.title.nativeElement);
+    this.outputString()
+  }
+
+  inputValue:string = "how's life is Going on?"
+  inputValue2:string = "how's life is 1 3 45 going on?"
+  outputString(){
+    const splitValue = this.inputValue.split(" ");
+     splitValue.forEach((el,index)=> {
+        if(splitValue[index] === 'on?'){
+           splitValue[index] = 'on';
+        }
+     })
+    // console.log(this.inputValue2)
+    console.log(this.inputValue2.match(/\S+/g))
+      console.log(this.splitText(this.text,this.reg))
+  }
+
+
+ splitText(text: string, pattern:any)  {
+    if (!text) {
+        return null
+    }
+    return text.match(pattern)
+}
+
+
+
+
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
@@ -6,8 +6,38 @@ import { Component, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css',
-  encapsulation:ViewEncapsulation.ShadowDom
+  // encapsulation:ViewEncapsulation.ShadowDom
 })
-export class ParentComponent {
+export class ParentComponent implements OnInit {
+
+  @Input()gettingValue!:string
+  inputValue2:string = "how's life is 1 3 45 going on?";
+   extractName = 'My name is cody';
+   extractFileName = "file_1.mp3 file_2.mp3 file.csv";
+   fileNameRegex = /(\w+)\.mp3/g
+    regex= /My name is ([a-z]+)/
+  log(){
+    console.log('log parent component')
+  }
+
+  ngOnInit(): void {
+    console.log(this.inputValue2.replace(/[^a-zA-Z]+/g," ").trim().split(' '));
+    // console.log(this.extractName.exec())
+
+    const match= this.regex.exec(this.extractName);
+    if(match){
+      console.log(match)
+    }
+
+     let getFileName = this.fileNameRegex.exec(this.extractFileName);
+     console.log(getFileName)
+     while(getFileName){
+      console.log(getFileName[1])
+      getFileName = this.fileNameRegex.exec(this.extractFileName);
+     }
+  }
+
+
+
 
 }
